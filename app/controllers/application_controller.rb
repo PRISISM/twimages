f706c5df0ab70661a1898e2e6c882bab0740278b
toashel <shelchean@gmail.com>
2016-02-12 22:34:45 +1300

    Change production.rb for Heroku deployment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
 
-  before_filter :loader
-
   def index
   end
 
@@ -12,12 +10,6 @@ class ApplicationController < ActionController::Base
   	if (params[:name][0] == '@') 
   		params[:name] = params[:name][1..-1]
   	end
-  end
-
-  private
-
-  def loader
-  	load 'config/initializers/twitter_credentials.rb'
   end
 
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,12 @@
 Rails.application.configure do
+  require 'twitter'
+
+  $twitter = Twitter::REST::Client.new do |config|
+    config.consumer_key = 'UnS4RLlfnA8jSqckNYzYReu5W'
+    config.consumer_secret = 'o8MHGErwcmJMH7lNoqOq8XP1BweuL2V4oBkIhwcfMrrIzFBZGO'
+    config.access_token = '74698390-UfUN9Dt5gauPWVjYjKP1xrH3n980lCJvKxkgX5vfZ'
+    config.access_token_secret = 'pXb9yokpH9Y8pwdz4g6EXM035NJORfRQAnMiAaSRouUkd'
+  end
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
