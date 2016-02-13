443f004f25aa01d8e47556a1d0b33ad5d25aa147
toashel <shelchean@gmail.com>
2016-02-13 18:00:08 +1300

    Add flash for invalid username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,20 @@ class ApplicationController < ActionController::Base
   	if (params[:name][0] == '@') 
   		params[:name] = params[:name][1..-1]
   	end
+
+    if !valid(params[:name])
+      flash[:notice] = "You have entered an invalid Twitter name."
+      redirect_to root_url
+    end
   end
 
+  def valid(name)
+    if (name.length > 15 || name.length == 0)
+      return false
+    elsif (name.match('\W')) # Check for non alphanumeric or underscore
+      return false
+    else
+      return true
+    end
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,2 @@
-require 'twitter-photos'
 module ApplicationHelper
-	class TwitterUser
-		attr_reader :client
-
-		def initialize
-			@user = new TwitterPhoto
-		end
-
-	end
-
 end
